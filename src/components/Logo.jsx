c10c49203d16cdb5951cf8e616a23e8aa96a335c
cherrydub <cherrydub@gmail.com>
2023-08-28 20:19:45 +0100

    final cleanup of starter files

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
 
-export default function Logo() {
+function Logo() {
   return (
     <Link to="/">
       <img src="/logo.png" alt="WorldWise logo" className={styles.logo} />
     </Link>
   );
 }
+
+export default Logo;
