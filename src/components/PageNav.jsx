71284ebefccb6568c008bfce1614679e533b71b3
cherrydub <cherrydub@gmail.com>
2023-08-28 20:17:03 +0100

    files reordered, styles added and most routes done;

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import style from "./PageNav.module.css";
+import Logo from "./Logo";
 
 export default function PageNav() {
   return (
     <nav className={style.nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Homepage</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={style.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,7 @@
-// import React from "react";
-
-import PageNav from "../components/PageNav";
-
 export default function PageNotFound() {
   return (
     <div>
-      <PageNav />
-      PageNotFound
+      <h1>Page not found 😢</h1>
     </div>
   );
 }
